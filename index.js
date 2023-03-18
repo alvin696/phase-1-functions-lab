a14aca5865fff2bba2eac729e8d869b207b0d5dc
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-03-18 11:11:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+
+// This function calculates the distance from the pickup location to the company headquarters in blocks
+function distanceFromHqInBlocks(pickupLocation) {
+    let hqLocation = 42;
+    let distanceInBlocks = Math.abs(hqLocation - pickupLocation);
+    return distanceInBlocks;
+  }
+  
+  // This function calculates the distance from the pickup location to the company headquarters in feet
+  function distanceFromHqInFeet(pickupLocation) {
+    let distanceInFeet = distanceFromHqInBlocks(pickupLocation) * 264;
+    return distanceInFeet;
+  }
+  
+  // This function calculates the distance travelled in feet between the pickup location and the dropoff location
+  function distanceTravelledInFeet(pickupLocation, dropoffLocation) {
+    let distanceInFeet = Math.abs(dropoffLocation - pickupLocation) * 264;
+    return distanceInFeet;
+  }
+  
+  // This function calculates the fare price based on the distance travelled
+  function calculatesFarePrice(pickupLocation, dropoffLocation) {
+    let distanceInFeet = distanceTravelledInFeet(pickupLocation, dropoffLocation);
+    let farePrice;
+  
+    if (distanceInFeet <= 400) {
+      farePrice = 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      farePrice = (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      farePrice = 25;
+    } else if (distanceInFeet > 2500) {
+      farePrice = 'cannot travel that far';
+    }
+  
+    return farePrice;
+  }
+  
